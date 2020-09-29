4173efb6582856bb0a1dbc97faec36c350308dc8
Atin Agnihotri <atinagnihotri@gmail.com>
2020-09-30 00:01:58 +0530

    Material Design

--- app/src/main/java/com/atinagnihotri/uisandbox/MainActivity.java
+++ app/src/main/java/com/atinagnihotri/uisandbox/MainActivity.java
@@ -14,16 +14,27 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity{
 
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.action_float);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Fab Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/atinagnihotri/uisandbox/XMLTradeMarkActivity.java
@@ -0,0 +1,14 @@
+package com.atinagnihotri.uisandbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class XMLTradeMarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_x_m_l_trade_mark);
+    }
+}
