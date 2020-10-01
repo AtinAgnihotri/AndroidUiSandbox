bcf7a9023845b71374664703fc904a31bf66a45d
Atin Agnihotri <atinagnihotri@gmail.com>
2020-10-01 23:29:28 +0530

    RecyclerView 2

--- app/src/main/java/com/atinagnihotri/uisandbox/ContactsRecyclerViewAdapter.java
+++ app/src/main/java/com/atinagnihotri/uisandbox/ContactsRecyclerViewAdapter.java
@@ -1,8 +1,12 @@
 package com.atinagnihotri.uisandbox;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,29 +15,47 @@ import java.util.ArrayList;
 
 public class ContactsRecyclerViewAdapter extends RecyclerView.Adapter<ContactsRecyclerViewAdapter.ViewHolder> {
 
+    private Context context;
+    private ArrayList<Contact> contacts = new ArrayList<>(); // Need to initialise it, otherwise will get a NullPointerException
+
     public ArrayList<Contact> getContacts() {
         return contacts;
     }
 
     public void setContacts(ArrayList<Contact> contacts) {
         this.contacts = contacts;
-        notifyDataSetChanged();
+        notifyDataSetChanged(); // Notify adapter that dataset has changed, so it can refresh
     }
 
-    private ArrayList<Contact> contacts = new ArrayList<>();
 
-    public ContactsRecyclerViewAdapter() {
+    public ContactsRecyclerViewAdapter(Context context) {
+        this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // The purpose of this method is to create an instance of our viewholder class for every item in our recycler view
+        // ViewGroup parent of all the Layout types
+        View view = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.contacts_list_item,
+                parent, // Tells where do you want to attach your View obj
+                false // False, since we are already passing the parent. Can set to true if we passed Null in prev arg
+        ); // Wil inflate a layout
+
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.txtName.setText(contacts.get(position).getName());
+        holder.recyclerParent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Calling " + contacts.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,9 +66,11 @@ public class ContactsRecyclerViewAdapter extends RecyclerView.Adapter<ContactsRe
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView txtName;
+        private RelativeLayout recyclerParent;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
+            recyclerParent = itemView.findViewById(R.id.recyclerParent);
         }
     }
 }
--- app/src/main/java/com/atinagnihotri/uisandbox/MainActivity.java
+++ app/src/main/java/com/atinagnihotri/uisandbox/MainActivity.java
@@ -1,11 +1,15 @@
 package com.atinagnihotri.uisandbox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity{
@@ -18,6 +22,26 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         contactsRecyclerView = findViewById(R.id.contactsRecyclerView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+
+        contacts.add(new Contact("John Shepard", "cdr.shepard@anmail.org", "https://vignette.wikia.nocookie.net/massfanon/images/2/2c/WpPh0Mlckaov-0.png"));
+        contacts.add(new Contact("Firelord Zuko", "firelord@firenation.com", "https://64.media.tumblr.com/342efcc204d04788e53d02c9118fae2c/tumblr_ocdu0yb7tr1sjodmuo1_1280.png"));
+        contacts.add(new Contact("Rick Sanchez", "c_137@wubaluba.dubdub", "https://upload.wikimedia.org/wikipedia/en/a/a6/Rick_Sanchez.png"));
+        contacts.add(new Contact("Eleanor Shellstrop", "shell.drops@good.place", "https://img.nbc.com/sites/nbcunbc/files/files/images/2016/9/01/2016-0823-TheGoodPlace-KristenBell-1050x1050-CV.jpg"));
+        contacts.add(new Contact("Elvi Okoye", "elvi.oky@charterroyal.com", "https://vignette.wikia.nocookie.net/expanse/images/b/b7/Dy5Vi_MWkAQcykr.jpg_large.jpg"));
+
+        ContactsRecyclerViewAdapter adapter = new ContactsRecyclerViewAdapter(this);
+        adapter.setContacts(contacts);
+
+        contactsRecyclerView.setAdapter(adapter);
+//        contactsRecyclerView.setLayoutManager(new LinearLayoutManager(this)); // Vertical Linear
+//        contactsRecyclerView.setLayoutManager(new LinearLayoutManager(
+//                this,
+//                LinearLayoutManager.HORIZONTAL,
+//                false)
+//        ); // Horizontal Linear
+        contactsRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
     }
 
 
